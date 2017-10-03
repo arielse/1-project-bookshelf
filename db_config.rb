b03123a2bcc5ec4f7892c7207e65a13b5df9c90e
arielse <ariel@bottledup.net>
2017-10-04 10:06:08 +1100

    folder structure established

---
+++ db_config.rb
@@ -0,0 +1,9 @@
+require "active_record"
+
+options = {
+  adapter: 'postgresql',
+  database: 'bookshelf'
+}
+
+# this is how to activate, must be at the bottom of the page.
+ActiveRecord::Base.establish_connection(options)
---
+++ main.rb
@@ -0,0 +1,6 @@
+require "sinatra"
+require "sinatra/reloader"
+# PG is the library used to do commands in PostGreSQL like "conn" below
+require "pg"
+require "pry"
+require_relative "db_config"
