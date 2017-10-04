be52c3d34e20cfc863fd34e56cb10dc02a5a3c86
arielse <ariel@bottledup.net>
2017-10-04 14:19:49 +1100

    login/logout functionality established

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Test user login details:
+###Test user login details:
+
+#### User one:
 username: arthurdent
 email: arthurdent@gmail.com
 password: fortytwo
+
+#### User two:
+username: zaphod
+email: beeblebrox@galacticpresident.gov
+password: trillian
--- main.rb
+++ main.rb
@@ -9,12 +9,49 @@ require_relative "models/review"
 require_relative "models/book"
 require_relative "models/shelf"
 
+enable :sessions # Sinatra allows sessions
+
+# helpers is from Sinatra, it allows you to use methods across the whole site.
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+  def logged_in?
+    if current_user
+      true
+    else
+      false
+    end
+  end
+end
+
 get '/' do
 
   erb :index
 end
 
 get '/login' do
-
+  @message = ''
   erb :login
+end
+
+# start session after login
+post '/session' do
+  # find user
+  user = User.find_by(email: params[:email])
+  # if found user
+  if user && user.authenticate(params[:password])
+    # successful create session then redirect1
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @message = 'incorrect email or password'
+    erb :login
+  end
+end
+
+delete '/session' do
+  # deletes the session id, which logs the person out
+  session[:user_id] = nil
+  redirect '/login'
 end
