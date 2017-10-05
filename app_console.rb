b8fb382213fe87d555c0eba7d6cdcb8be3fd2391
arielse <ariel@bottledup.net>
2017-10-05 15:51:16 +1100

    app_console updated

--- app_console.rb
+++ app_console.rb
@@ -5,5 +5,7 @@ require_relative "models/user"
 require_relative "models/book"
 require_relative "models/shelf"
 
-# binding.pry
+binding.pry
 puts "yay"
+
+# heroku run bash -this connects to the heroku server and you can do all the usual stuff like ruby app_console.rb
